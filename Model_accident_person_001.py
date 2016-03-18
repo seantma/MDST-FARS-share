a8f591efb5da5602549d1e7ea0841c7af89d191d
SeanM <tehsheng@gmail.com>
2016-03-18 12:46:09 -0400

    tweaking tianpei's code

--- Model_accident_person_001.py
+++ Model_accident_person_001.py
@@ -110,8 +110,8 @@ print("Saving the model")
 bst.save_model('./models/xgb_acc_per.model')
 bst.dump_model('./models/xgb_raw_acc_per.txt')
 
-#dtrain.save_binary('./models/binary/dtrain.buffer')
-#deval.save_binary('./models/binary/deval.buffer')
+dtrain.save_binary('./models/binary/dtrain.buffer')
+deval.save_binary('./models/binary/deval.buffer')
 
 #get_ipython().magic(u'xdel dtrain')
 #get_ipython().magic(u'xdel deval')
@@ -148,7 +148,7 @@ preds = bst.predict(dtest)
 
 # In[25]:
 
-#dtest.save_binary('./models/binary/dtest.buffer')
+dtest.save_binary('./models/binary/dtest.buffer')
 
 
 # In[27]:
