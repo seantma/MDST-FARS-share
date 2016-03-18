f8dd046bee3805f9d3ccf04824349d82f412f3d4
SeanM <tehsheng@gmail.com>
2016-03-18 07:20:41 -0400

    added qsub code for Tianpei model

--- Model_accident_person_001.py
+++ Model_accident_person_001.py
@@ -6,12 +6,9 @@
 
 # In[1]:
 
-get_ipython().magic(u'reset')
-get_ipython().magic(u'matplotlib inline')
 import os
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import seaborn as sns
 import sqlite3
 import xgboost as xgb
